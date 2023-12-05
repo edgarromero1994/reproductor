beae2edd98addae580ffbfb4c0c7a6737a069823
edgarromero1994 <edgarromerocuc94@gmail.com>
2023-12-05 07:03:43 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, {  } from "react";
+import AudioPlayer from "./componentes/AudioPlayer";
+
+
+function App() {
+
+  
+  return (
+    <div className="App">
+    <header className="App-header">
+      <h1>Reproductor de audio</h1>
+    </header>
+    <main>
+       <AudioPlayer/> 
+    </main>
+  </div>
+
+  );
+}
+
+export default App;
